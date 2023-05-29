6f4d2e4c9acc298874263f8ce1a2d6d9b3e6d617
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-05-29 10:14:34 +0200

    not much

--- server/config/index.js
+++ server/config/index.js
@@ -11,11 +11,11 @@ const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
 module.exports = (app) => {
   app.set("trust proxy", 1);
 
-  // app.use(
-  //   cors({
-  //     origin: [FRONTEND_URL]
-  //   })
-  // );
+  app.use(
+    cors({
+      origin: [FRONTEND_URL]
+    })
+  );
 
   app.use(logger("dev"));
   app.use(express.json());
