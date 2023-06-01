bda16ee1f9b4a4eac56079e7df1eb59d1578d698
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-06-01 15:44:30 +0200

    profile edit

--- server/routes/profile.routes.js
+++ server/routes/profile.routes.js
@@ -11,4 +11,18 @@ router.get("/:profile_id/getOneProfile", (req, res, next) => {
         .catch(err => next(err))
 })
 
+router.put("/:profile_id/edit", (req, res, next) => {
+
+    const { profile_id } = req.params
+
+    //const { editedProfileData } = req.body
+    const { username, email, avatar } = req.body
+    User
+        .findByIdAndUpdate(profile_id, { username, email, avatar }/*editedProfileData*/)
+        .then(response => res.json(response))
+        .catch(err => next(err))
+})
+
+
+
 module.exports = router;
