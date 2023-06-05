5b346d5c03f9bf38ba7b80538812b5780583f113
Manuel Pérez <mperezpradosjose@gmail.com>
2023-06-05 16:52:53 +0200

    a

--- server/controllers/profile.controllers.js
+++ server/controllers/profile.controllers.js
@@ -18,14 +18,11 @@ const GetTickets = (req, res, next) => {
     const { profile_id } = req.params
     const { ticket, combo } = req.body;
     const packs = { ticket, combo };
-<<<<<<< HEAD
 
     if (ticket === undefined || combo === undefined) {
         res.status(400).json({ errorMessages: ["Provide a ticket and combo."] });
         return;
     }
-=======
->>>>>>> 2e602f1 (done)
 
     User
         .findByIdAndUpdate(profile_id, { $push: { packs: { ticket, combo } } }, { new: true })
@@ -42,85 +39,57 @@ const EditProfile = (req, res, next) => {
         .then(response => res.json(response))
         .catch(err => next(err))
 
-<<<<<<< HEAD
-}
-
-<<<<<<< HEAD
-const GetPackDetails = (req, res, next) => {
-    const { pack_id } = req.params
-    User
-        .findById(pack_id)
-        .populate('packs.ticket')
-        .populate('packs.combo')
-        .then(response => res.json(response))
-        .catch(err => next(err))
-}
-
-const AddWatchlistId = (req, res, next) => {
-
-    const { movie_id } = req.params
-    const { profile_id } = req.body
-
-    User
-        .isDuplicateMovie(profile_id, movie_id)
-        .then(exists => {
-            if (!exists) {
-                User
-                    .findByIdAndUpdate(profile_id, { $push: { watchList: movie_id } }, { new: true })
-                    .then(response => res.json(response))
-            } else {
-                res.json(null)
-            }
-        })
-        .catch(err => next(err))
-
-}
-
-const removeMovieFromWatchlist = (req, res, next) => {
-
-    const { movie_id } = req.params
-    const { profile_id } = req.body
 
 
-    User
-        .findByIdAndUpdate(profile_id, { $pull: { watchList: movie_id } })
-=======
-}
+    const GetPackDetails = (req, res, next) => {
+        const { pack_id } = req.params
+        User
+            .findById(pack_id)
+            .populate('packs.ticket')
+            .populate('packs.combo')
+            .then(response => res.json(response))
+            .catch(err => next(err))
+    }
 
-const GetPackDetails = (req, res, next) => {
-    const { pack_id } = req.params
-    User
-        .findById(pack_id)
->>>>>>> 2e602f1 (done)
-        .then(response => res.json(response))
-        .catch(err => next(err))
-}
-=======
-// const GetPackDetails = (req, res, next) => {
-//     const { pack_id } = req.params
-//     User
-//         .findById(pack_id)
-//         .populate('packs.ticket')
-//         // .populate('packs.combo')
-//         .then(response => res.json(response))
-//         .catch(err => next(err))
-// }
->>>>>>> 18ead63 (monday commit)
+    const AddWatchlistId = (req, res, next) => {
+
+        const { movie_id } = req.params
+        const { profile_id } = req.body
+
+        User
+            .isDuplicateMovie(profile_id, movie_id)
+            .then(exists => {
+                if (!exists) {
+                    User
+                        .findByIdAndUpdate(profile_id, { $push: { watchList: movie_id } }, { new: true })
+                        .then(response => res.json(response))
+                } else {
+                    res.json(null)
+                }
+            })
+            .catch(err => next(err))
+
+    }
+
+    const removeMovieFromWatchlist = (req, res, next) => {
+
+        const { movie_id } = req.params
+        const { profile_id } = req.body
 
 
-<<<<<<< HEAD
-=======
+        User
+            .findByIdAndUpdate(profile_id, { $pull: { watchList: movie_id } })
+            .then(response => res.json(response))
+            .catch(err => next(err))
+    }
 
->>>>>>> 2e602f1 (done)
-module.exports = {
-    GetOneProfile,
-    GetTickets,
-    EditProfile,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    GetPackDetails,
-    AddWatchlistId,
-    removeMovieFromWatchlist
+    module.exports = {
+        GetOneProfile,
+        GetTickets,
+        EditProfile,
+        GetPackDetails,
+        AddWatchlistId,
+        removeMovieFromWatchlist
 
     }
 
