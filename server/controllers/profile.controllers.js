347df1e8900d4844c89ec1643ebfa1f21d56f2e5
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-06-06 09:24:34 +0200

    Done

--- server/controllers/profile.controllers.js
+++ server/controllers/profile.controllers.js
@@ -18,14 +18,11 @@ const GetTickets = (req, res, next) => {
     const { profile_id } = req.params
     const { ticket, combo } = req.body;
     const packs = { ticket, combo };
-<<<<<<< HEAD
 
     if (ticket === undefined || combo === undefined) {
         res.status(400).json({ errorMessages: ["Provide a ticket and combo."] });
         return;
     }
-=======
->>>>>>> 2e602f1 (done)
 
     User
         .findByIdAndUpdate(profile_id, { $push: { packs: { ticket, combo } } }, { new: true })
@@ -41,11 +38,8 @@ const EditProfile = (req, res, next) => {
         .findByIdAndUpdate(profile_id, { username, email, avatar })
         .then(response => res.json(response))
         .catch(err => next(err))
-
-<<<<<<< HEAD
 }
 
-<<<<<<< HEAD
 const GetPackDetails = (req, res, next) => {
     const { pack_id } = req.params
     User
@@ -84,48 +78,14 @@ const removeMovieFromWatchlist = (req, res, next) => {
 
     User
         .findByIdAndUpdate(profile_id, { $pull: { watchList: movie_id } })
-=======
 }
 
-const GetPackDetails = (req, res, next) => {
-    const { pack_id } = req.params
-    User
-        .findById(pack_id)
->>>>>>> 2e602f1 (done)
-        .then(response => res.json(response))
-        .catch(err => next(err))
-}
-=======
-// const GetPackDetails = (req, res, next) => {
-//     const { pack_id } = req.params
-//     User
-//         .findById(pack_id)
-//         .populate('packs.ticket')
-//         // .populate('packs.combo')
-//         .then(response => res.json(response))
-//         .catch(err => next(err))
-// }
->>>>>>> 18ead63 (monday commit)
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 2e602f1 (done)
 module.exports = {
     GetOneProfile,
     GetTickets,
     EditProfile,
-<<<<<<< HEAD
-<<<<<<< HEAD
     GetPackDetails,
     AddWatchlistId,
     removeMovieFromWatchlist
-=======
-    GetPackDetails
->>>>>>> 2e602f1 (done)
-=======
-    // GetPackDetails
->>>>>>> 18ead63 (monday commit)
 }
 
